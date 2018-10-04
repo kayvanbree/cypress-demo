a28829cf0f5471dbabdd5df6a6bebc2f85ee2feb
Kay van Bree <kayvanbree@gmail.com>
2018-10-04 18:41:23 +0200

    Write test (it fails!)

--- cypress/support/step_definitions/textfield.steps.js
+++ cypress/support/step_definitions/textfield.steps.js
@@ -1,17 +1,22 @@
 /* global Given, When, Then */
 
-Given(/^I am on the webpage$/, () => {
+const url = 'http://localhost:4204/';
 
+Given(/^I am on the webpage$/, () => {
+  cy.visit(url);
 });
 
 When(/^I type "(.*?)" in the text field$/, (text) => {
-
+  cy.get('[cy-data=textfield]')
+    .type(text);
 });
 
 When(/^I click send$/, (dock) => {
-
+  cy.get('[cy-data=submit]')
+    .click();
 });
 
 Then(/^the label should say "(.*?)"$/, (text) => {
-
+  cy.get('[cy-data=label]')
+    .contains(text);
 });
