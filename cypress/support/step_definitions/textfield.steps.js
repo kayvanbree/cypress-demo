bf2ec7d589405ac57b5765a644c925e2734c8ec0
Kay van Bree <kayvanbree@gmail.com>
2018-10-04 18:55:13 +0200

    Want our tests to fail first

--- cypress/support/step_definitions/textfield.steps.js
+++ cypress/support/step_definitions/textfield.steps.js
@@ -1,17 +1,3 @@
 /* global Given, When, Then */
 
-Given(/^I am on the webpage$/, () => {
 
-});
-
-When(/^I type "(.*?)" in the text field$/, (text) => {
-
-});
-
-When(/^I click send$/, (dock) => {
-
-});
-
-Then(/^the label should say "(.*?)"$/, (text) => {
-
-});
